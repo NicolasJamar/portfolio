f32a564b86079a8baa9a1028f74e45f108342ae6
NicolasJamar <nicolas.jamar@gmail.com>
2018-01-18 16:57:08 +0100

    h2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
   $('#lien').click(function(){
     // var card = $(this).attr('id'); // Page cible
-      $('html, body').animate( { scrollTop: $('#intro').offset().top }, 800 ); // Go
+      $('html, body').animate( { scrollTop: $('#main').offset().top }, 800 ); // Go
       return false;
 });
 
